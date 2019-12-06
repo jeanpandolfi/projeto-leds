f2934cc6bd9ecd4fe65c996947a8a53854ec5d07
jeanpandolfi <jean-pandolfi@hotmail.com>
2019-12-06 08:15:01 -0300

    Dando acesso aos métodos GET

--- src/main/java/com/example/projeto/leds/config/ResourceServerConfig.java
+++ src/main/java/com/example/projeto/leds/config/ResourceServerConfig.java
@@ -3,6 +3,7 @@ package com.example.projeto.leds.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -39,8 +40,8 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			.antMatchers("/pessoa").permitAll()
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/pessoa")
+			.permitAll()
 			.anyRequest().authenticated()
 			.and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
